cba0f198b6e0a8f81ce6f2dc3a4a7dd25f342337
Jiri <jvanek@redhat.com>
2021-05-23 20:26:52 +0200

    Search now do something

--- src/main/java/hudson/plugins/nested_view/NestedViewsSearch.java
+++ src/main/java/hudson/plugins/nested_view/NestedViewsSearch.java
@@ -1,39 +1,66 @@
 package hudson.plugins.nested_view;
 
 
-import hudson.search.*;
 
-import org.kohsuke.stapler.Ancestor;
+import hudson.search.Search;
+import hudson.search.SearchIndex;
+import hudson.search.SearchItem;
+import hudson.search.SearchResult;
+import hudson.search.SuggestedItem;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-    public class NestedViewsSearch extends Search {
-        private final static Logger LOGGER = Logger.getLogger(Search.class.getName());
+public class NestedViewsSearch extends Search {
 
+    private static class NestedViewsSearchResult implements SearchItem {
+        private final String searchName;
+        private final String searchUrl;
 
+        public String getSearchName() {
+            return searchName;
+        }
 
-        public NestedViewsSearch() {
-
+        public String getSearchUrl() {
+            return searchUrl;
         }
 
         @Override
-        public void doIndex(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
-            String query = req.getParameter("q");
-            if (query != null) {
-               // hits = normalSearch(req, query);
-              //  hits.addAll(manager.getHits(query, true));
-            }
-            //req.getView(this, "search-results.jelly").forward(req, rsp);
+        public SearchIndex getSearchIndex() {
+            return null;
         }
+
+        public NestedViewsSearchResult(String searchName, String searchUrl) {
+            this.searchName = searchName;
+            this.searchUrl = searchUrl;
+        }
+    }
+
+    private final static Logger LOGGER = Logger.getLogger(Search.class.getName());
+    private List<NestedViewsSearchResult> hits = new ArrayList<>();
+
+
+    public NestedViewsSearch() {
+
+    }
+
+    @Override
+    public void doIndex(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
+        String query = req.getParameter("q");
+        if (query != null) {
+            hits.add(new NestedViewsSearchResult("ccc", "../ddd"));
+            hits.add(new NestedViewsSearchResult("eee", "../fff"));
+        }
+        RequestDispatcher v = req.getView(this, "search-results.jelly");
+        v.forward(req, rsp);
+    }
 
         @Override
         public SearchResult getSuggestions(final StaplerRequest req, @QueryParameter final String query) {
@@ -57,5 +84,12 @@ import java.util.logging.Logger;
             return suggestedItems;
         }
 
-
+    public String getSearchHelp() throws IOException {
+        return "nvp help";
     }
+
+    public List<NestedViewsSearchResult> getHits() {
+        return hits;
+    }
+
+}
