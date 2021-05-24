b46a50a75f35872aa02207ed9e822ae091d775c6
Jiri <jvanek@redhat.com>
2021-05-24 20:00:47 +0200

    Perform real search with proper urls n results

--- src/main/java/hudson/plugins/nested_view/NestedViewsSearch.java
+++ src/main/java/hudson/plugins/nested_view/NestedViewsSearch.java
@@ -2,11 +2,15 @@ package hudson.plugins.nested_view;
 
 
 
+import hudson.model.FreeStyleProject;
+import hudson.model.TopLevelItem;
+import hudson.model.View;
 import hudson.search.Search;
 import hudson.search.SearchIndex;
 import hudson.search.SearchItem;
 import hudson.search.SearchResult;
 import hudson.search.SuggestedItem;
+import jenkins.model.Jenkins;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
@@ -15,19 +19,87 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
 
 public class NestedViewsSearch extends Search {
 
-    private static class NestedViewsSearchResult implements SearchItem {
+    private static class NamableWithClass {
+        private final String name;
+        private final String fullPath;
+        private Object item;
+
+        private NamableWithClass(Object item, String name, String fullPath) {
+            this.item = item;
+            this.name = name;
+            this.fullPath = fullPath;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getFullPath() {
+            return fullPath;
+        }
+
+        public String getUsefulName() {
+            if (item instanceof FreeStyleProject) {
+                return name;
+            } else {
+                if (item instanceof NestedView) {
+                    return fullPath + "/";
+                } else {
+                    return fullPath;
+                }
+            }
+        }
+
+        public String getUrl() {
+            if (item instanceof FreeStyleProject) {
+                return "../job/" + name;
+            } else {
+                return "../" + getFullPath().replace("/", "/view/");
+            }
+        }
+    }
+
+    private final List<NamableWithClass> all = new ArrayList(1000);
+
+    public NestedViewsSearch() {
+        Jenkins j = Jenkins.get();
+        for (TopLevelItem ti : j.getItems()) {
+            if (ti instanceof FreeStyleProject) {
+                all.add(new NamableWithClass(ti, ti.getName(), ti.getName()));
+            }
+        }
+        addViewsRecursively(j.getViews(), "/");
+    }
+
+    private void addViewsRecursively(Collection<View> views, String s) {
+        for (View v : views) {
+            if (v instanceof NestedView) {
+                NestedView nw = (NestedView) v;
+                all.add(new NamableWithClass(v, v.getViewName(), s + v.getViewName()));
+                addViewsRecursively(((NestedView) v).getViews(), s + v.getViewName() + "/");
+            } else {
+                all.add(new NamableWithClass(v, v.getViewName(), s + v.getViewName()));
+            }
+        }
+    }
+
+    private static class NestedViewsSearchResult implements SearchItem, Comparable {
         private final String searchName;
         private final String searchUrl;
 
+        @Override
         public String getSearchName() {
             return searchName;
         }
 
+        @Override
         public String getSearchUrl() {
             return searchUrl;
         }
@@ -41,23 +113,33 @@ public class NestedViewsSearch extends Search {
             this.searchName = searchName;
             this.searchUrl = searchUrl;
         }
+
+        @Override
+        public String toString() {
+            return searchName;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            return this.toString().length() - o.toString().length();
+        }
     }
 
     private final static Logger LOGGER = Logger.getLogger(Search.class.getName());
     private List<NestedViewsSearchResult> hits = new ArrayList<>();
 
 
-    public NestedViewsSearch() {
-
-    }
-
     @Override
     public void doIndex(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
         String query = req.getParameter("q");
         if (query != null) {
-            hits.add(new NestedViewsSearchResult("ccc", "../ddd"));
-            hits.add(new NestedViewsSearchResult("eee", "../fff"));
+            for (NamableWithClass item : all) {
+                if (item.getFullPath().contains(query)) {
+                    hits.add(new NestedViewsSearchResult(item.getUsefulName(), item.getUrl()));
+                }
+            }
         }
+        Collections.sort(hits);
         RequestDispatcher v = req.getView(this, "search-results.jelly");
         v.forward(req, rsp);
     }
@@ -65,22 +147,11 @@ public class NestedViewsSearch extends Search {
         @Override
         public SearchResult getSuggestions(final StaplerRequest req, @QueryParameter final String query) {
             SearchResult suggestedItems = super.getSuggestions(req, query);
-            suggestedItems.add(new SuggestedItem(new SearchItem() {
-                @Override
-                public String getSearchName() {
-                    return "aaaa";
+            for (NamableWithClass item : all) {
+                if (item.getFullPath().contains(query)) {
+                    suggestedItems.add(new SuggestedItem(new NestedViewsSearchResult(item.getUsefulName(), item.getUrl())));
                 }
-
-                @Override
-                public String getSearchUrl() {
-                    return "bbbbb";
-                }
-
-                @Override
-                public SearchIndex getSearchIndex() {
-                    return null;
-                }
-            }));
+            }
             return suggestedItems;
         }
 
--- src/main/java/hudson/plugins/nested_view/NestedViewsSearchFactory.java
+++ src/main/java/hudson/plugins/nested_view/NestedViewsSearchFactory.java
@@ -4,11 +4,13 @@ import hudson.Extension;
 import hudson.search.Search;
 import hudson.search.SearchFactory;
 import hudson.search.SearchableModelObject;
+import jenkins.model.Jenkins;
+
+import javax.inject.Inject;
 
 
 @Extension
 public class NestedViewsSearchFactory extends SearchFactory {
-
 
     @Override
     public Search createFor(final SearchableModelObject owner) {
