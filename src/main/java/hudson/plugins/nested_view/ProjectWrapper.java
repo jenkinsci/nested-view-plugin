bbf9921e198bd2b6efdbf0c95c35c6db8a5e8a89
Jiri Vanek <jvanek@redhat.com>
2023-03-10 12:11:57 +0100

    Project/build show swothces are now honored

--- src/main/java/hudson/plugins/nested_view/ProjectWrapper.java
+++ src/main/java/hudson/plugins/nested_view/ProjectWrapper.java
@@ -28,7 +28,7 @@ public class ProjectWrapper {
     public ProjectWrapper(Optional<AbstractProject> project, boolean multiline, boolean projectInfo, int stats, int last, int builds, NestedViewsSearch.Query nvrSearch) {
         this.project = project;
         this.multiline = multiline;
-        this.projectInfo = projectInfo;        
+        this.projectInfo = projectInfo;
         this.stats = stats;
         this.last = last;
         this.builds = builds;
@@ -38,65 +38,87 @@ public class ProjectWrapper {
     //FIXME repalce String b somethign what can harbor link to exact build and maybe collored results
     public List<String> getDetails() {
         if (project.isPresent()) {
-            //-P
-            String projectInfo = "" +
-                    " builds count: " + project.get().getBuilds().size() + ", " +
-                    " is running  : " + project.get().isBuilding() + ", " +
-                    " in queue    : " + project.get().isInQueue() + ", " +
-                    " disabled    : " + project.get().isDisabled();
-            //-L/L0 -all
-            //-L1
-            BuildDetails lastBuild = specifiedBuild(" last build           : ", project.get().getLastBuild());
-            //-L2
-            BuildDetails lastStable = specifiedBuild(" last stable build    : ", project.get().getLastStableBuild());
-            //-L3
-            BuildDetails lastSuc = specifiedBuild(" last success build   : ", project.get().getLastSuccessfulBuild());
-            //-L4
-            BuildDetails lastUnst = specifiedBuild(" last unstable build  : ", project.get().getLastUnstableBuild());
-            //-L5
-            BuildDetails lastFail = specifiedBuild(" last failed build    : ", project.get().getLastFailedBuild());
-            //-L6
-            BuildDetails lastUnsuc = specifiedBuild(" last unsuccess build : ", project.get().getLastUnsuccessfulBuild());
-            //-L7
-            BuildDetails lastComp = specifiedBuild(" last completed build : ", project.get().getLastCompletedBuild());
-
-            Iterator it = project.get().getBuilds().iterator();
-            //-B , -Bn n builds to past
-            List<BuildDetails> buildsList = new ArrayList<>();
-            //-S, -Sn - stats
-            Map<Result, Integer> summ = new HashMap<>();
-            int i = Math.max(builds, stats);
-            while (it.hasNext()) {
-                if (i<=0) {
-                    break;
+            List<String> result = new ArrayList<>();
+            if (projectInfo) {
+                //-P
+                String projectInfo = "" +
+                        " builds count: " + project.get().getBuilds().size() + ", " +
+                        " is running  : " + project.get().isBuilding() + ", " +
+                        " in queue    : " + project.get().isInQueue() + ", " +
+                        " disabled    : " + project.get().isDisabled();
+                result.add(projectInfo);
+            }
+            if (last >= 0) {
+                String s = String.valueOf(last);
+                //-L/L0 -all
+                if (s.contains("1")) {//-L1
+                    BuildDetails lastBuild = specifiedBuild(" last build           : ", project.get().getLastBuild());
+                    result.add(lastBuild.toString());
                 }
-                Object q = it.next();
-                if (q instanceof AbstractBuild) {
-                    AbstractBuild b = (AbstractBuild) q;
-                    buildsList.add(buildToString(b));
-                    Integer r = summ.getOrDefault(b.getResult(), 0);
-                    r = r + 1;
-                    summ.put(b.getResult(), r);
+                if (s.contains("2")) {//-L2
+                    BuildDetails lastStable = specifiedBuild(" last stable build    : ", project.get().getLastStableBuild());
+                    result.add(lastStable.toString());
                 }
-                i--;
+                if (s.contains("3")) {//-L3
+                    BuildDetails lastSuc = specifiedBuild(" last success build   : ", project.get().getLastSuccessfulBuild());
+                    result.add(lastSuc.toString());
+                }
+                if (s.contains("4")) {//-L4
+                    BuildDetails lastUnst = specifiedBuild(" last unstable build  : ", project.get().getLastUnstableBuild());
+                    result.add(lastUnst.toString());
+                }
+                if (s.contains("5")) {//-L5
+                    BuildDetails lastFail = specifiedBuild(" last failed build    : ", project.get().getLastFailedBuild());
+                    result.add(lastFail.toString());
+                }
+                if (s.contains("6")) {//-L6
+                    BuildDetails lastUnsuc = specifiedBuild(" last unsuccess build : ", project.get().getLastUnsuccessfulBuild());
+                    result.add(lastUnsuc.toString());
+                }
+                if (s.contains("7")) {//-L7
+                    BuildDetails lastComp = specifiedBuild(" last completed build : ", project.get().getLastCompletedBuild());
+                    result.add(lastComp.toString());
+                }
+            }
+            if (builds>=0 || stats >= 0) {
+                Iterator it = project.get().getBuilds().iterator();
+                //-B , -Bn n builds to past
+                List<BuildDetails> buildsList = new ArrayList<>();
+                //-S, -Sn - stats
+                Map<Result, Integer> summ = new HashMap<>();
+                int i1 = builds;
+                int i2 = stats;
+                while (it.hasNext()) {
+                    if (i1 <= 0 && i2 <=0) {
+                        break;
+                    }
+                    Object q = it.next();
+                    if (q instanceof AbstractBuild) {
+                        AbstractBuild b = (AbstractBuild) q;
+                        if (i1>=0) {
+                            buildsList.add(buildToString(b));
+                        }
+                        if (i2>=0) {
+                            Integer counter = summ.getOrDefault(b.getResult(), 0);
+                            counter = counter + 1;
+                            summ.put(b.getResult(), counter);
+                        }
+                    }
+                    i1--;
+                    i2--;
+                }
+                if (stats >= 0) {
+                    result.add(summ.entrySet().stream().map(a -> a.getKey() + ": " + a.getValue() + "x").collect(Collectors.joining(", ")));
+                }
+                if (builds>=0) {
+                    result.addAll(buildsList.stream().map(a -> a.toString()).collect(Collectors.toList()));
+                }
             }
             //-m multiline
-            List<String> r = new ArrayList<>();
-            if (multiline) {
-                r.add("");
+            if (multiline && result.size() > 0) {
+                result.add(0, "");
             }
-
-            r.add(projectInfo);
-            r.add(lastBuild.toString());
-            r.add(lastStable.toString());
-            r.add(lastSuc.toString());
-            r.add(lastUnst.toString());
-            r.add(lastFail.toString());
-            r.add(lastUnsuc.toString());
-            r.add(lastComp.toString());
-            r.add(summ.entrySet().stream().map(a -> a.getKey() + ": " + a.getValue() + "x").collect(Collectors.joining(", ")));
-            r.addAll(buildsList.stream().map(a -> a.toString()).collect(Collectors.toList()));
-            return r;
+            return result;
         } else {
             return Arrays.asList("N/A");
         }
