6525070b5873861615774e0e2288f79a99e5d1a4
Jiri Vanek <jvanek@redhat.com>
2024-10-16 14:02:59 +0200

    JENKINS-73971  Extract event handlers in NestedViewsSearch/search-results.jelly

---
+++ src/main/resources/hudson/plugins/nested_view/NestedViewsSearch/searchscripts.js
@@ -0,0 +1,17 @@
+        // <![CDATA[
+
+function nvwp_showHide_button_2_1_show() {
+  document.getElementById('button_expand_2_1').style.display = 'none';
+  document.getElementById('button_collapse_2_1').style.display = 'inline';
+  document.getElementById('details_2_1').style.display = 'block';
+  return false;
+}
+function nvwp_showHide_button_2_1_hide() {
+  document.getElementById('button_collapse_2_1').style.display = 'none';
+  document.getElementById('details_2_1').style.display = 'none';
+  document.getElementById('button_expand_2_1').style.display = 'inline';
+  return false;
+}
+
+        // ]]>
+
