660b2b2a733659c4db117ec14d780b89df731ff5
Jiri <jvanek@redhat.com>
2024-10-17 18:26:15 +0200

    Adding onClick dynamically
    moving adjunct after creation of elements

--- src/main/resources/hudson/plugins/nested_view/NestedViewsSearch/searchscripts.js
+++ src/main/resources/hudson/plugins/nested_view/NestedViewsSearch/searchscripts.js
@@ -13,5 +13,13 @@ function nvwp_showHide_button_2_1_hide() {
   return false;
 }
 
+const exEl = document.getElementById("button_expand_2_1");
+exEl.addEventListener("click", nvwp_showHide_button_2_1_show);
+
+const colEl = document.getElementById("button_collapse_2_1");
+colEl.addEventListener("click", nvwp_showHide_button_2_1_hide);
+
+
+
         // ]]>
 
